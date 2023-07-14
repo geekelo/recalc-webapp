740da685ffc34d25e7c77dd50e5e750629b2ffc2
geekelo <efuelight12@gmail.com>
2023-07-14 14:54:20 +0100

    Create structure and add styles for calculator

---
+++ src/componenets/calculator.js
@@ -0,0 +1,30 @@
+function CreateStructure() {
+  return (
+    <section className="calculatorContain">
+      <p className="displaySection">0</p>
+      <main className="btnContainer">
+        <p className="buttons btnSet">AC</p>
+        <p className="buttons btnSet">+/-</p>
+        <p className="buttons btnSet">%</p>
+        <p className="buttons btnSet">&#247;</p>
+        <p className="buttons btnSet">7</p>
+        <p className="buttons btnSet">8</p>
+        <p className="buttons btnSet">9</p>
+        <p className="buttons btnSet">&#10005;</p>
+        <p className="buttons btnSet">4</p>
+        <p className="buttons btnSet">5</p>
+        <p className="buttons btnSet">6</p>
+        <p className="buttons btnSet">-</p>
+        <p className="buttons btnSet">1</p>
+        <p className="buttons btnSet">2</p>
+        <p className="buttons btnSet">3</p>
+        <p className="buttons btnSet">+</p>
+        <p className="buttons btnSet zeroBtn">0</p>
+        <p className="buttons btnSet">.</p>
+        <p className="buttons btnSet">=</p>
+      </main>
+    </section>
+  );
+}
+
+export default CreateStructure;
